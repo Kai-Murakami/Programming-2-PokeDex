2acdbdb1c486b3eb7005d8eda2cf290ebb3770c0
Kai-Murakami <kaiaxlmurakami@gmail.com>
2025-03-30 22:17:22 -0700

    Update Pokedex.cpp

--- Pokedex.cpp
+++ Pokedex.cpp
@@ -259,7 +259,8 @@ void abilitySearch()
 
 void personalLibrary() 
 {
-    int id;
+   char back;
+   int id;
     string personalname;
     string type;
     int total;
@@ -281,27 +282,67 @@ if (choice == 'V' || choice == 'v')
 {
     pokedex.listPokemons();
     cout<<"\n";
+     cout << "Press B to go back: ";
+    cin >> back;
+    while (back != 'b' && back != 'B')
+{
+    cout << "Please Enter a Valid Option." << endl;
+    cout << "Press B to go back: ";
+    cin >> back;
+}
 }
 else if (choice == 'R' || choice == 'r')
 { 
 cout<<"\n=====================================\n    *** Pokedex Custom Entry *** \n=====================================\nFirst please enter your pokemon's ID: ";
-cin>>id;
+while (!(cin >> id)) {
+        cin.clear(); // Clear the error flag
+        cin.ignore(10000, '\n'); // Ignore invalid input up to the next newline
+        cout << "Invalid input. Please enter an integer: ";
+    }
 cout<<"Now Enter the name of your pokemon: ";
 cin>>personalname;
 cout<<"Now enter the type of your pokemon: ";
-cin>>type;
+while (!(cin >> type)) {
+        cin.clear(); // Clear the error flag
+        cin.ignore(10000, '\n'); // Ignore invalid input up to the next newline
+        cout << "Invalid input. Please enter an integer: ";
+    }
 cout<<"Now enter the HP of your pokemon: ";
-cin>>hp;
+while (!(cin >> hp)) {
+        cin.clear(); // Clear the error flag
+        cin.ignore(10000, '\n'); // Ignore invalid input up to the next newline
+        cout << "Invalid input. Please enter an integer: ";
+    }
 cout<<"Now enter the Attack of your pokemon: ";
-cin>>atk;
+while (!(cin >> atk)) {
+        cin.clear(); // Clear the error flag
+        cin.ignore(10000, '\n'); // Ignore invalid input up to the next newline
+        cout << "Invalid input. Please enter an integer: ";
+    }
 cout<<"Now enter the Defense of your pokemon: ";
-cin>>def;
+while (!(cin >> def)) {
+        cin.clear(); // Clear the error flag
+        cin.ignore(10000, '\n'); // Ignore invalid input up to the next newline
+        cout << "Invalid input. Please enter an integer: ";
+    }
 cout<<"Now enter the Special Attack of your pokemon: ";
-cin>>spatk;
+while (!(cin >> spatk)) {
+        cin.clear(); // Clear the error flag
+        cin.ignore(10000, '\n'); // Ignore invalid input up to the next newline
+        cout << "Invalid input. Please enter an integer: ";
+    }
 cout<<"Now enter the Special Defense of your pokemon: ";
-cin>>spdef;
+while (!(cin >> spdef)) {
+        cin.clear(); // Clear the error flag
+        cin.ignore(10000, '\n'); // Ignore invalid input up to the next newline
+        cout << "Invalid input. Please enter an integer: ";
+    }
 cout<<"Now enter the Speed of your pokemon: ";
-cin>>speed;
+while (!(cin >> speed)) {
+        cin.clear(); // Clear the error flag
+        cin.ignore(10000, '\n'); // Ignore invalid input up to the next newline
+        cout << "Invalid input. Please enter an integer: ";
+    }
 cout<<"\nPokemon succesfully added!\n\n";
 pokedex.addPokemon(PokemonLibrary(personalname, type, id, total, hp, atk, def, spatk, spdef, speed));
 }
